b6b8601652f7306290877f7557445a6b133859a8
Igor Litovchenko <igorlitovcenko@Igors-MacBook-Air.local>
2024-03-09 18:42:42 +0200

    gitignore update

--- business/target/classes/db/migration/oracle/V2__Insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_detail VALUES (UUID '035670f0-5d53-4691-b72c-4f969a10edc1', 'Bob', 'Tompson', 'b-login-t2');
-INSERT INTO user_detail VALUES (UUID '035670f0-5d53-4691-b72c-4f969a10edc2', 'Jack', 'Simpson', 'j-login-t2');
